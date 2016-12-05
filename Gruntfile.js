ae4b68d32926b2d6120a31bcdbb58897c02b54d8
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-12-06 00:42:45 +0530

    Initial commit
    Chalk — Default Color Defined.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,3 +56,10 @@ var glob       = require('glob');
 // -----------------------------
 var os         = require('os');
 var fs         = require('fs');
+
+// Default color defined.
+// ----------------------
+var noop       = chalk.red;
+var yeep       = chalk.green;
+var okay       = chalk.blue;
+var boop       = chalk.gray;
