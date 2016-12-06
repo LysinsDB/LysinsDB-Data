c05771f43e0242cedc3c49641657d43359c6c5f6
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-12-06 18:20:19 +0530

    Initial commit
    Apache License | Build with SEED™ — Örebro.

---
+++ tasks/options/watch.js
@@ -0,0 +1,30 @@
+/*!
+ * --------------
+ * LysinsDB®-Data
+ * --------------
+ * Data of LysinsDB — A manually curated dedicated database of Lysins.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 06-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-watch
+ * @build     : SEED™ — Örebro
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "LysinsDB®-Data".
+ * ___________________________________________________________________________
+ */
